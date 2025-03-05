a0e52a8241afaa6238c6aa1e904aafc1e0f2a171
varnikhasree123 <varnik024@gmail.com>
2025-03-05 06:27:57 +0530

    Update ChatComponent.js

--- src/app/components/ChatComponent.js
+++ src/app/components/ChatComponent.js
@@ -3,14 +3,14 @@ import React from "react";
 import Chatbot from "react-chatbot-kit";
 import "react-chatbot-kit/build/main.css";
 
-import ActionProvider from "../Actionprovider";  // Class import
+import ActionProvider from "../Actionprovider";
 import MessageParser from "../MessageParser";
 import config from "../config";
 
 function ChatComponent() {
     return (
       <div className="chat-container">
-        <div className="chat-header">Cooking Assistant</div>
+        <div className="chat-header">Disability Employment Assistant</div>
         <div className="chatbot-wrapper">
           <Chatbot
             config={config}
@@ -20,6 +20,6 @@ function ChatComponent() {
         </div>
       </div>
     );
-  }
-  
-  export default ChatComponent;
+}
+
+export default ChatComponent;
